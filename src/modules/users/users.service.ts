1092c805db79629f29b7260c5214278f15a11b4b
Amit Koren <30484152+amitko92@users.noreply.github.com>
2025-03-16 00:11:27 +0200

    check if the user(email) already exists in the database.

--- src/modules/users/users.service.ts
+++ src/modules/users/users.service.ts
@@ -13,15 +13,20 @@ export class UsersService {
 
     constructor(@InjectModel(User.name) private userModel: Model<User>) {}
 
-    signUp(signUpDto: SignUpDto): Promise<User> {
+    async signUp(signUpDto: SignUpDto): Promise<User> {
 
         // 1. validate the user data.
         if(signUpDto.password !== signUpDto.confirmPassword) {
             throw new Error('passwords do not match');
         }
 
-        // 2. check if the user already exists in the database.
-
+        // 2. check if the user(email) already exists in the database.
+        const existsUser = await this.userModel.findOne({email: signUpDto.email});
+        
+        if(existsUser) {
+            throw new Error('user already exists');
+        }
+        
         // 3. create the iniall data of the new user.
         const creationDate = moment().format('DD/MM/YYYY');
 
@@ -31,7 +36,7 @@ export class UsersService {
             creationDate
         });
 
-        return newUser.save();
+        return await newUser.save();
     }
 
     signIn(signIpDto: SignInDto): string {
